199ad880f657b716387146aab1f100d0e07b45e5
Manikant S Adaragunchi <shankaradaragunchi126@gmail.com>
2025-04-23 21:02:33 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int info;
+    struct node *link;
+};
+typedef struct node N;
+N *getnode();
+void push(N **, int);
+int pop(N **);
+void display(N *);
+main()
+{
+    int item, choice, save;
+    N *first = NULL;
+    while (1)
+    {
+        printf("\nEnter 1:push\n2:pop\n3:display\n4:exit\n");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter item\n");
+            scanf("%d", &item);
+            push(&first, item);
+            break;
+        case 2:
+            save = pop(&first);
+            if (save == 0)
+                printf("Nothing to delete\n");
+            else
+                printf("%d is deleted\n", save);
+
+            break;
+        case 3:
+            display(first);
+            break;
+        case 4:
+            exit(0);
+        }
+    }
+}
