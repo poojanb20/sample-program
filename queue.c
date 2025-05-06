22e62ab73088af545bdff8f951cb1030e385ffd9
Manikant S Adaragunchi <shankaradaragunchi126@gmail.com>
2025-05-06 21:39:40 +0530

    Updated main block

--- queue.c
+++ queue.c
@@ -18,3 +18,59 @@ void display();
  
 //glob
 struct queue q;
+int main()
+{
+    q.front = -1;
+    q.rear = -1;
+    int user_choice, data;
+    char user_active = 'Y';
+ 
+    while (user_active == 'Y' || user_active == 'y')
+    {
+        printf("\n--------Queue Program------\n");
+        printf("\n1. Enqueue");
+        printf("\n2. Dequeue");
+        printf("\n3. Display");
+        printf("\n4. Exit");
+ 
+        printf("\n\nEnter your choice: ");
+        scanf("%d", &user_choice);
+ 
+        switch(user_choice)
+        {
+            case 1:
+                printf("\nEnter Data: ");
+                scanf("%d", &data);
+                enqueue(data);
+                break;
+         case 2:
+                if (!isEmpty())
+                {
+                    data = dequeue();
+                    printf("\n* %d was removed!\n", data);
+                }
+                else
+                {
+                    printf("\nQueue is Empty!\n");
+                }
+                break;
+ 
+            case 3:
+                display();
+                printf("\n");
+                break;
+ 
+            case 4:
+                printf("\n\tProgram was terminated!\n\n");
+                return 1;
+ 
+            default:
+                printf("\n\tInvalid Choice\n");
+        }
+ 
+        printf("\nDo You want to continue? ");
+        fflush(stdin);
+        scanf(" %c", &user_active);
+    }
+    return 0;
+}
